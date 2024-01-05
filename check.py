3ea85b118a6fb8073c35780e4a78b6673251c920
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-01-05 16:33:20 +0900

    Now more Pythonic

---
+++ check.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from assertpy.assertpy import assert_that
+from python_config import Config
+
+conf = Config("config.json")
+k400_root = Path(conf.kinetics400.root)
+ext = conf.kinetics400.ext
+files = set()
+
+assert_that(k400_root).is_directory().is_readable()
+print("Scanning directories...")
+
+for split in "train", "val", "test":
+    for action in (k400_root / "videos" / split).iterdir():
+        files.update({
+            file.stem.strip()
+            for file in action.iterdir()
+            if file.is_file() and file.suffix == ext
+        })
+
+for split in "train", "val", "test":
+    file_list_path = Path("CMN/kinetics-100") / f"{split}.list"
+
+    assert_that(file_list_path).is_file().is_readable()
+
+    with open(file_list_path) as f:
+        file_list = {file.strip().split("/")[1] for file in f.readlines()}
+
+    total = len(file_list)
+    found = len(files & file_list)
+    not_found = len(file_list - files)
+
+    print("Total:", total)
+    print("Found:", found)
+    print("Not found:", not_found, end="\n\n")
