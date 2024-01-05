e308fe6dd099eaa2ae54c58ae766efda84845a88
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-01-05 17:29:22 +0900

    Added the main build.py

---
+++ build.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+
+from assertpy.assertpy import assert_that
+from python_config import Config
+from tqdm import tqdm
+
+conf = Config("config.json")
+k400_root = Path(conf.kinetics400.root)
+k100_root = Path(conf.kinetics100.root)
+ext = conf.kinetics400.ext
+files = {}
+
+assert_that(k400_root).is_directory().is_readable()
+print("Scanning directories...")
+
+bar = tqdm()
+
+for split in "train", "val", "test":
+    for action in (k400_root / "videos" / split).iterdir():
+        bar.set_description(f"{split}/{action.name}")
+        files.update(
+            {
+                file.stem.strip(): file
+                for file in action.iterdir()
+                if file.is_file() and file.suffix == ext
+            }
+        )
+
+print("\nCreating symlinks...")
+
+created = 0
+
+for split in "train", "val", "test":
+    file_list_path = Path("CMN/kinetics-100") / f"{split}.list"
+
+    assert_that(file_list_path).is_file().is_readable()
+
+    with open(file_list_path) as f:
+        file_list = f.readlines()
+
+    for file in file_list:
+        action, stem = file.strip().split("/")
+
+        if stem not in files.keys():
+            continue
+
+        src = files[stem]
+        dst = (k100_root / split / action / stem).with_suffix(ext)
+
+        dst.parent.mkdir(parents=True, exist_ok=True)
+        bar.set_description(f"{split}/{action}/{stem}")
+        os.symlink(src, dst)
+
+        created += 1
+
+bar.close()
+print("Created symlinks:", created)
